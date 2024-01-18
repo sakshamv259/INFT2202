9a5c8cc7960ed356933c9c5018653ce40f652afd
Sakshamv259 <Sakshamv259@gmail.com>
2024-01-17 21:42:48 -0500

    ice2

--- ICE2/scripts/event-flow.js
+++ ICE2/scripts/event-flow.js
@@ -3,7 +3,7 @@
 function showElement(e) {  
   // e.cancelBubble = true;                           // prevent the bubbling up through the elements with a click on the innermost element
   // e.stopPropagation();                             // prevents bubbling and capturing
-  // alert("Using 'e.target': " + e.target.innerHTML);  // event is the element that recieved the click
+  // alert("Using 'e.target': " + e.target.innerHTML);  // event is the element that received the click
   alert("Using 'this': " + this.innerHTML);          // this is the element that fired the event  
   // alert("This is only only link element node...or is it?")
 }
@@ -14,16 +14,16 @@ function showElement(e) {
 
 // function grandparent(e) {
 //   alert("This is on the grandparent list element node.")
-// }
+
 //#endregion
 
 //#region EVENT BUBBLING 
 
 // because clicking on a child within a parent necessarily clicks on the parent, if both have a click event wired up, that click event fires even if the events are different
-// let link_bubble = document.getElementById("link");
-// link.addEventListener('click', showElement, false);
+let link_bubble = document.getElementById("link");
+link_bubble.addEventListener('click', showElement, false);
 
-// event bubbling when using the different events
+// event bubbling when using different events
 // let item_bubble = document.getElementById('item');
 // item.addEventListener('click', showElement, false);
 
@@ -31,14 +31,13 @@ function showElement(e) {
 // let list_bubble = document.getElementById('list');
 // list.addEventListener('click', showElement, false)
 
-// event bubbling when using the different events
+// event bubbling when using different events
 // let item_bubble2 = document.getElementById('item');
 // item.addEventListener('click', writeSomething, false);
 
 // event bubbling when using different events
 // let list_bubble2 = document.getElementById('list');
 // list.addEventListener('click', grandparent, false)
-
 //#endregion
 
 //#region EVENT CAPTURING
@@ -55,4 +54,3 @@ let list2_capture = document.getElementById("list2");
 list2_capture.addEventListener('click', showElement, true);
 
 //#endregion
-
